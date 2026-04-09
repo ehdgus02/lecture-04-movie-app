cb541a27ac0930de52a5351e9f0307ba9b4147f7
ehdgus02 <ehdgus02kk@naver.com>
2026-04-09 16:17:59 +0900

    수신 오류로 인한 API 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import { useEffect, useState } from "react";
 import MovieCard from "./components/MovieCard.jsx";
+import "./style/global.css";   // global.css 를 불러오고 싶다
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+// express에서는 process.env.환경변수KEY
+// VITE를 사용하는 React에서는 import.meta.env.환경변수KEY
+
 
 function App() {
     const [loading, setLoading] = useState(true); // 불러올 때의 loading 상태를 관리함 state
     const [movies, setMovies] = useState([]); // 불러올 무비 목록을 저장할 state
 
     useEffect(() => {
-        fetch("https://imdb.iamidiotareyoutoo.com/search?q=2025&lsn=1&v=1")
+        fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=ko-KR&page=1`)
             .then(res => res.json())
             .then(json => {
-                setMovies(json.description);
+                setMovies(json.results);
                 setLoading(false);
             })
             .catch(error => {
@@ -25,10 +31,11 @@ function App() {
                 <div>
                     {movies.map(value => (
                         <MovieCard
-                            key={value["#IMDB_ID"]}
-                            poster={value["#IMG_POSTER"]}
-                            title={value["#TITLE"]}
-                            actors={value["#ACTORS"]}
+                            key={value.id}
+                            poster={value.poster_path}
+                            title={value.title}
+                            overview={value.overview}
+                            vote_average={value.vote_average}
                         />
                     ))}
                 </div>
